baa57c838a0b1771b69011252cb9a0529babfb2e
Luma <65837288+Luma-exe@users.noreply.github.com>
2025-04-20 03:08:31 +1000

    2nd Push

--- client/circuitBreaker/circuit.lua
+++ client/circuitBreaker/circuit.lua
@@ -598,7 +598,7 @@ end
 exports('runMiniGame', runMiniGame)
 exports('runDefaultMiniGameFromDifficulty', runDefaultMiniGameFromDifficulty)
 exports('runDefaultRandom', runDefaultMiniGame)
-exports('StartCircuitBreaker ', runMiniGame)
+exports('StartCircuitBreaker', runMiniGame)
 
 if config.DebugCommands then
     RegisterCommand('testcircuit', function(source, args, rawCommand)
