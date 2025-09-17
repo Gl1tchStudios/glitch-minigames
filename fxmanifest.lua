0d6b3729730c558f95d236561038181e81565dc0
Slurpy <116125747+SlurpyDevv@users.noreply.github.com>
2025-09-17 16:59:46 +1000

    Bump version from 1.0.4 to 1.0.5

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Luma in collaboration with Glitch Studios'
 description 'Glitch Minigames'
-version '1.0.4'
+version '1.0.5'
 
 client_script {
     'client/customMinigames/client.lua',
